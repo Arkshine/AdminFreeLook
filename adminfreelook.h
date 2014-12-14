676fcf7ab9a0694d701514bfbff7e08955a217cf
Arkshine <hv.contact@gmail.com>
2014-12-14 16:06:11 +0100

    Add licence headers

--- adminfreelook.h
+++ adminfreelook.h
@@ -1,3 +1,15 @@
+//
+// AMX Mod X, based on AMX Mod by Aleksander Naszko ("OLO").
+// Copyright (C) The AMX Mod X Development Team.
+//
+// This software is licensed under the GNU General Public License, version 3 or higher.
+// Additional exceptions apply. For full license details, see LICENSE.txt or visit:
+//     https://alliedmods.net/amxmodx-license
+
+//
+// AdminFreeLook Module
+//
+
 #ifndef ADMINFREELOOK_H
 #define ADMINFREELOOK_H
 
