1457f55cea323e97a37c03380b4b46d241d97f32
Arkshine <hv.contact@gmail.com>
2014-12-14 21:25:35 +0100

    Update configure.py (reverted from commit 441b8420574b617146f3abae93ac04e23f8b7b8e)

--- support/configure.py
+++ support/configure.py
@@ -8,6 +8,6 @@ run.options.add_option('--enable-debug', action='store_const', const='1', dest='
 run.options.add_option('--enable-optimize', action='store_const', const='1', dest='opt', help='Enable optimization')
 run.options.add_option('--no-mysql', action='store_true', default=False, dest='disable_mysql', help='Disable building MySQL extension')
 run.options.add_option('--breakpad-dump', action='store_true', dest='breakpad_dump', default=False, help='Dump and upload breakpad symbols')
-run.options.add_option('--metamod', type='string', dest='metamod_path', default='../metamod-am', help='Path to Metamod source code')
-run.options.add_option('--hlsdk', type='string', dest='hlsdk_path', default='../hlsdk', help='Path to the HLSDK')
+run.options.add_option('--metamod', type='string', dest='metamod_path', default='../../metamod-am', help='Path to Metamod source code')
+run.options.add_option('--hlsdk', type='string', dest='hlsdk_path', default='../../hlsdk', help='Path to the HLSDK')
 run.Configure()
